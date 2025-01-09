e5da599fe7ed6619e2e679a82aa6184933bf02dd
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-01-09 22:08:10 +0545

    feat: add FlowerSvg component with wave calculations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
-import { LotusSvg } from './lotus-svg.tsx';
+import { LotusSvg, calcInnerWave, calcOuterWave, polarToCartesian } from './lotus-svg.tsx';
 
 function SideBar() {
   return (
@@ -43,6 +44,37 @@ function SideBar() {
 //   );
 // }
 
+function FlowerSvg() {
+  const [data, setPathData] = useState<string[]>([]);
+  useEffect(() => {
+    const rScale = 40; // Scale factor for r
+
+    const numPoints = 360; // Number of points to plot
+
+    for (let i = 0; i <= numPoints; i++) {
+      const theta = (i / numPoints) * 2 * Math.PI; // θ from 0 to 2π
+      // let m = 3 + 2 + 8 * Math.abs(Math.cos(6 * theta + Math.PI / 2)) * Math.abs(Math.cos(3 * theta)) + 2 * (0.25 - Math.abs(Math.cos(3 * theta + Math.PI / 2)));
+
+      const outerWave = calcOuterWave(theta);
+      const innerWave = calcInnerWave(theta);
+
+      const a = Math.abs(Math.cos(3 * theta));
+      const b = 2 * (0.25 - innerWave);
+      const r = 3 + a + b / (2 + 8 * outerWave);
+      // const r = 5 + 3 * Math.cos(6 * theta);
+
+      const { x, y } = polarToCartesian(r * rScale, theta);
+
+      setPathData((prev) => [...prev, `${i === 0 ? 'M' : 'L'} ${x} ${y}`]);
+    }
+  }, []);
+  return (
+    <svg width='500' height='500' viewBox='-250 -250 500 500'>
+      <path d={data.join(' ')} fill='none' stroke='#FF2E98' stroke-width='2' />
+    </svg>
+  );
+}
+
 function App() {
   return (
     <section
@@ -54,6 +86,7 @@ function App() {
     >
       <main className={'bg-[#333] flex flex-grow'}>
         <LotusSvg />
+        <FlowerSvg />
         {/*<div className={'grid place-content-center row-span-1 col-span-1 '}>*/}
         {/*  <LotusSvg />*/}
         {/*  <LotusSvg />*/}
--- src/lotus-svg.tsx
+++ src/lotus-svg.tsx
@@ -1,11 +1,19 @@
 import { useControls } from 'leva';
 import { useEffect, useState } from 'react';
 
-function polarToCartesian(r: number, theta: number) {
+export function polarToCartesian(r: number, theta: number) {
   return {
     x: r * Math.cos(theta),
     y: r * Math.sin(theta),
   };
+}
+
+export function calcOuterWave(value: number) {
+  return Math.abs(Math.cos(6 * value + Math.PI / 2));
+}
+
+export function calcInnerWave(value: number) {
+  return Math.abs(Math.cos(3 * value + Math.PI / 2));
 }
 type NumberType<T extends string> = { [key in T]: number };
 function calculatePoints({
@@ -13,16 +21,17 @@ function calculatePoints({
   petalFrequency,
   squashFactor,
   rScale,
-}: NumberType<'numPoints' | 'petalFrequency' | 'squashFactor' | 'rScale'>) {
+  shape,
+}: NumberType<'numPoints' | 'petalFrequency' | 'squashFactor' | 'rScale' | 'shape'>) {
   const pathData: string[] = [];
   for (let i = 0; i <= numPoints; i++) {
     const theta = (i / numPoints) * 2 * Math.PI; // θ from 0 to 2π
 
-    const outerWave = Math.abs(Math.cos(6 * petalFrequency * theta + Math.PI / 2));
-    const innerWave = Math.abs(Math.cos(3 * petalFrequency * theta + Math.PI / 2));
+    const outerWave = calcOuterWave(petalFrequency * theta);
+    const innerWave = calcInnerWave(petalFrequency * theta);
 
     const a = Math.abs(Math.cos(3 * petalFrequency * theta));
-    const b = 2 * (0.9 - innerWave);
+    const b = 2 * (shape - innerWave);
     let r = 3 + a + b / (2 + 8 * outerWave);
 
     // Apply squash factor
@@ -38,9 +47,10 @@ function calculatePoints({
 }
 export function LotusSvg() {
   const [data, setData] = useState<string[]>([]);
-  const { petalsMul, scale } = useControls({
+  const { petalsMul, scale, shape } = useControls({
     petalsMul: { value: 1, step: 1, min: 1, max: 20 },
     scale: { value: 50, min: 50 },
+    shape: { value: 0.9, min: 0, max: 2 },
   });
   useEffect(() => {
     const rScale = scale; // Scale factor for r
@@ -50,9 +60,9 @@ export function LotusSvg() {
     // Adjust these values to change the number and shape of petals
     const petalFrequency = petalsMul; // Increase this to add more petals
     const squashFactor = 1 / (petalsMul * 2); // Adjust this to squash (< 1) or stretch (> 1) the petals
-    const pathData = calculatePoints({ rScale, numPoints, petalFrequency, squashFactor });
+    const pathData = calculatePoints({ rScale, numPoints, petalFrequency, squashFactor, shape });
     setData(pathData);
-  }, [petalsMul, scale]);
+  }, [petalsMul, scale, shape]);
   return (
     <section className={'flex-grow grid place-content-center col-start-1 row-start-1'}>
       <svg width='500' height='500' viewBox='-250 -250 500 500'>
